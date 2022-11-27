ab33a8d7d649807d72a9b5e6034388cc4973d724
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2022-11-27 22:11:43 +0900

    variable

---
+++ helloworld/variable/variableSample.py
@@ -0,0 +1,14 @@
+name1 = 'hoge'
+name2: str = 'fuga'
+
+# hoge
+print(name1)
+# hoge <class 'str'>
+print(name1, type(name1))
+
+age = 27
+
+# 27 <class 'int'>
+print(age, type(age))
+# hoge 27
+print(name1, age)
